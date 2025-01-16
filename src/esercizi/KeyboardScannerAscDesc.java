212898c46dc5419deb2bc5fd6673c100301f87b0
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-01-16 23:44:00 +0100

    BUG fixed - getArraySize() - now infinite loop for get a positive integer

--- src/esercizi/KeyboardScannerAscDesc.java
+++ src/esercizi/KeyboardScannerAscDesc.java
@@ -1,17 +1,15 @@
 package esercizi;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class KeyboardScannerAscDesc {
 
 	public void run() {
 		
-		int arraySize = getArraySize(); // BUG -> Sistema il -1 in caso di stringa (METTI RICHIESTA INFINITA FINO A QUANDO NON RICEVI UN INT)
+		int arraySize = getArraySize("Inserisci il numero di elementi che deve avere l'array, e premi \"Enter\"");
 		System.out.println(arraySize);
 		
-		int[] arrayElements = getArrayElements(arraySize);
+//		int[] arrayElements = getArrayElements(arraySize);
 
 		
 		
@@ -73,17 +71,35 @@ public class KeyboardScannerAscDesc {
 //        });
 	}
 	
-	private int getArraySize() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Inserisci il numero di elementi che deve avere l'array, e premi \"Enter\"");
+	
+	/* Questo metodo ha il compito di richiedere un input intero positivo, ovvero
+	 * la dimensione dell'array.
+	 * Se ciò che viene fornito dall'utente non è di tipo int positivo verrà
+	 * richiesto all'infinito. */
+	public int getArraySize(String prompt) {
 		
-		try {
-			return scanner.nextInt();
-		} catch (Exception e) {
-			return -1;
-		}
+		Scanner sc = new Scanner(System.in);
+		String errorMessage = "\nERROR:\nInput non valido. Per favore, inserisci un numero intero positivo.\n";
+		
+		// Finchè non ottengo un int positvo
+		while (true) {
+		    System.out.println(prompt);
 
+		    if (sc.hasNextInt()) {
+		        int inInt = sc.nextInt(); // Leggo il numero intero
+		        if (inInt > 0) {
+		            return inInt; // Se positivo, ritorno il valore
+		        } else {
+		            System.out.println(errorMessage);
+		            // Non consumo ulteriori token, dato che inInt è già stato letto
+		        }
+		    } else {
+		        System.out.println(errorMessage);
+		        sc.next(); // Consuma il token non valido
+		    }
+		}
 	}
+
 	
 	private int[] getArrayElements(int n) {
 		int[] myArray = new int[n];
